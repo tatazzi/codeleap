18a3c4f7abe862bd9005c06fbc2bc847b02810b3
tatazzi <otaviol.gon@gmail.com>
2023-04-13 17:28:51 -0300

    :fire: change the name of the files

--- src/screens/Login/Login.tsx
+++
@@ -1,7 +0,0 @@
-import React, {FC} from 'react';
-import {View} from 'react-native';
-import {LoginProps} from './types';
-
-export const Login: FC<LoginProps> = ({}) => {
-  return <View>ola</View>;
-};
